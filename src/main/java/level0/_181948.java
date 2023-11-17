a1b6dd7f9f7a1ae103f133d5cc4418c49bda30f6
deok-beom <baek940dog@gmail.com>
2023-11-17 17:59:46 +0900

    Solve: level#0, no.181948

---
+++ src/main/java/level0/_181948.java
@@ -0,0 +1,11 @@
+package level0;
+
+/*
+ * 특수문자 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181948
+ */
+public class _181948 {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
