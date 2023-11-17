2d5897f7f25456c204739423b206b915f9e0ea00
deok-beom <baek940dog@gmail.com>
2023-11-17 18:00:08 +0900

    Solve: level#0, no.181942

---
+++ src/main/java/level0/_181942.java
@@ -0,0 +1,17 @@
+package level0;
+
+/*
+ * 문자열 섞기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181942
+ */
+public class _181942 {
+    public String solution(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str1.length(); i++) {
+            sb.append(str1.charAt(i));
+            sb.append(str2.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
