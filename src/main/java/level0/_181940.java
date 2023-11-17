9327e535ef706c61f0551e54b3ad63b6a14cde24
deok-beom <baek940dog@gmail.com>
2023-11-17 18:00:14 +0900

    Solve: level#0, no.181940

---
+++ src/main/java/level0/_181940.java
@@ -0,0 +1,11 @@
+package level0;
+
+/*
+ * 문자열 곱하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181940
+ */
+public class _181940 {
+    public String solution(String my_string, int k) {
+        return my_string.repeat(k);
+    }
+}
