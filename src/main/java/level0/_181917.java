60f1b7926bc26a5ed5803b79442c3c1eb6ffd6a1
deok-beom <baek940dog@gmail.com>
2023-11-30 18:06:43 +0900

    Solve: level#0, no.181917

---
+++ src/main/java/level0/_181917.java
@@ -0,0 +1,11 @@
+package level0;
+
+/*
+ * 간단한 논리 연산
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181917
+ */
+public class _181917 {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        return (x1 || x2) && (x3 || x4);
+    }
+}
